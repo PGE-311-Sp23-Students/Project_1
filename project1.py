1b72e2c155563c41e810eb3f8080a8889e96be6c
cedar-ma <103262367+cedar-ma@users.noreply.github.com>
2023-03-22 11:01:47 -0500

    Add files via upload

--- project1.py
+++ project1.py
@@ -5,6 +5,8 @@ Created on Mon Feb 27 11:01:09 2023
 @author: bchan
 """
 
+# IO
+from hdf5storage import loadmat
 
 # Data structures and analysis
 import numpy as np
@@ -12,7 +14,6 @@ import numpy as np
 # Visualization
 import matplotlib.pyplot as plt
 import pyvista as pv
-
 
 # This line reads information from the file into a 3D numpy array called img.
 # The dimensions of the entire volume are stated below and used in reshape function call.
@@ -22,7 +23,6 @@ nx = 100
 
 img = np.fromfile('fracture.raw', dtype=np.uint8)
 img = img.reshape(nz,ny,nx)
-
 
 
 
@@ -36,15 +36,15 @@ img = img.reshape(nz,ny,nx)
 # Let the function save image in .png format using the specific choice of color and orientation.
 # Bonus: figure out how to save enough .png frames to create a video (animation of the frames)
 ###################################
-pl = pv.Plotter()
-pl.set_background(color='w')
 
-vtk_pore = pv.wrap(img)
-pore_contours = vtk_pore.contour(isosurfaces=[0.5])
 
-pl.add_mesh(pore_contours, opacity=0.9, color=(0/255, 0.25, 252/255))
 
-pl.show()
+
+
+
+
+
+
         
 
 #%%
@@ -58,17 +58,14 @@ pl.show()
 # histogram and 2D colorplot of aperture field (imshow) if plot_a = 'y').
 #####################################
 
-aperature = np.zeros((img.shape[1], img.shape[2]))
 
-for j in range(img.shape[1]):
-    for i in range(img.shape[2]):
-        aperature[j, i] = np.count_nonzero(img[:, j, i] == 0)
-        
-plt.hist(aperature.flatten())
 
-plt.figure(dpi=500)
-plt.imshow(aperature)
-plt.colorbar()
+
+
+
+
+
+
 
 #%%
 #####################################
@@ -77,8 +74,12 @@ plt.colorbar()
 # that computes and returns the Minimum, Maximum of any aperture field.
 # Execute this function using above aperture field a.
 ####################################
-def aperture_statistics(field):
-    return np.amin(field), np.amax(field)
+
+
+
+
+
+
 
 
 #%%
@@ -89,16 +90,11 @@ def aperture_statistics(field):
 # Execute this function using above aperture field a.
 ###################################
 
-def aperture_mean(field):
-    aperature_size = field.shape[0] * field.shape[1]
-    
-    aperature_sum = 0
-    for j in range(field.shape[1]):
-        for i in range(field.shape[0]):
-            aperature_sum += field[i, j]
-            
-    aperature_mean = aperature_sum / aperature_size
-    return aperature_mean
+
+
+
+
+        
 
 
 #%%
@@ -109,15 +105,13 @@ def aperture_mean(field):
 # Execute this function using above aperture field a.
 ###################################
 
-def roughness_coeff(field):
-    roughness_sum = 0
-    for j in range(field.shape[1] - 1):
-        for i in range(field.shape[0]):
-            roughness_sum += (field[i, j+1] - field[i, j]) ** 2
-            
-    aperature_size = field.shape[0] * field.shape[1]        
-    roughness_coeff = np.sqrt(roughness_sum / aperature_size)
-    return roughness_coeff
+
+
+
+
+        
+        
+
 
 
 #%%
@@ -127,18 +121,14 @@ def roughness_coeff(field):
 # that calculates and returns the tortuosity (eq. 3 from paper in repo)
 # Execute this function using above aperture field a.
 ###################################
-def tortuosity(field):
-    tortuosity_sum = 0
-    tmp = np.zeros(field.shape[0])
-    
-    for i in range(field.shape[0]):
-        tmp[i] = 0
-        for j in range(field.shape[1] - 1):
-            tmp[i] += np.sqrt((field[i,j+1]-field[i,j]) ** 2 + 1) 
-            
-        tortuosity_sum += tmp[i] / field.shape[0]
-    tortuosity = tortuosity_sum / field.shape[1]
-    return tortuosity
+
+
+
+
+
+
+
+
 
 #%%
 ####################################
@@ -148,11 +138,11 @@ def tortuosity(field):
 # where 2*B is equal to mean aperture calculated in Problem 4.
 # Further, compare volumetric flux to Darcy's law to calculate permeability k of the "parallel plate" fracture with the
 # same aperture as the mean (calculated in Problem 4)
-
 ###################################
-def perm(field):
-    B = aperture_mean(field) /2
-    
-    perm = (B ** 2)/ 3
-    return perm
+
+
+
+
+
+
 
